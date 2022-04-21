e826dd4d49dba0bc87407776e2c3fa58ad2e1b87
Quentin Rataud <quentin.rataud@epita.fr>
2022-04-21 21:13:34 +0200

    Fix Case Struct, add comments

--- src/case.hh
+++ src/case.hh
@@ -2,9 +2,9 @@
 
 struct Case
 {
-    type_case type;
-    etat_barriere barriere;
-    etat_nid nid;
-    pigeon_debug pigeon;
-    int papy;
+    etat_case etat;          ///< Position, type, constructible et pain
+    etat_barriere barriere;  ///< Informations d'état d'une barrière
+    etat_nid nid;            ///< Informations d'état d'un nid
+    pigeon_debug pigeon;     ///< Informations d'état du pigeon de debug
+    int papy_tours_restants; ///< Tours restants avant la pose d'un pain
 };
