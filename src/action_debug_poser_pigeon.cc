da717b94852249afd4d322d35582dfad47ff9ea1
Jeanne Cai <jeanne.cai@epita.fr>
2022-05-11 20:14:42 +0200

    ADD: action: debug poser pigeon: check the position on the map (check); put the pigeon on the cell (apply_on)

--- src/action_debug_poser_pigeon.cc
+++ src/action_debug_poser_pigeon.cc
@@ -2,14 +2,17 @@
 // Copyright (c) 2012-2020 Association Prologin <association@prologin.org>
 
 #include "actions.hh"
+#include "position.hh"
 
 int ActionDebugPoserPigeon::check(const GameState& st) const
 {
-    // FIXME
-    return 0;
+    if (!inside_map(pos_))
+        return POSITION_INVALIDE;
+
+    return OK;
 }
 
 void ActionDebugPoserPigeon::apply_on(GameState* st) const
 {
-    // FIXME
+    st->get_map().get_cell(pos_).pigeon = pigeon_;
 }
