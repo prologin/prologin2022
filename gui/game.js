a55bb0c02ccde077eec80dfb32d125e0ca964441
Simon Scatton <simon.scatton@epita.fr>
2022-05-18 12:54:52 +0200

    gui: game: added pixiJS skeleton and default game

---
+++ gui/game.js
@@ -0,0 +1,66 @@
+export class Game {
+
+    constructor() {
+        this.app = new PIXI.Application({ width: 800, height: 800 });
+    }
+
+    setupDocument(document) {
+        return document.body.appendChild(this.app.view);
+    }
+
+    action_avancer() {
+
+    }
+
+    action_grandir() {
+
+    }
+
+    action_construire_buisson() {
+
+    }
+
+    action_creuser_tunnel() {
+
+    }
+
+    pigeon_debug() {
+
+    }
+
+    error() {
+
+    }
+
+    info_score() {
+
+    }
+
+    info_spawn() {
+
+    }
+
+
+    info_recup_pains() {
+
+    }
+
+    info_deposer_pains() {
+    }
+
+    info_lacher_pain() {
+    }
+
+    info_ouvrir_barriere() {
+    }
+
+    info_fermer_barriere() {
+    }
+
+    info_division() {
+    }
+
+    info_dispersion() {
+    }
+
+}
