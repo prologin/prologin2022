3a2b876d7fb6d9349a7ac6f7c03c85fdbdcaf2b1
Simon Scatton <simon.scatton@epita.fr>
2022-05-19 17:48:17 +0200

    gui: added texture loading and unit and map matrix

--- gui/game.js
+++ gui/game.js
@@ -31,7 +31,22 @@ export class Game {
 
 
     constructor() {
-        this.app = new PIXI.Application({ width: 800, height: 800 });
+        this.app = new PIXI.Application({width: WINDOW_WIDTH, height: WINDOW_HEIGHT});
+        this.units = generateMatrix();
+        this.map = generateMatrix();
+        this.textures = {
+            old: [new PIXI.Texture.from("assets/chars/old/old_1.png"),
+                new PIXI.Texture.from("assets/chars/old/old_2.png"),
+                new PIXI.Texture.from("assets/chars/old/old_3.png"),
+                new PIXI.Texture.from("assets/chars/old/old_4.png"),
+                new PIXI.Texture.from("assets/chars/old/old_1.png"),],
+            grass: [new PIXI.Texture.from("assets/environment/grass/grass_1.png"),
+                new PIXI.Texture.from("assets/environment/grass/grass_2.png"),
+                new PIXI.Texture.from("assets/environment/grass/grass_3.png")],
+            pigeon: new PIXI.Texture.from("assets/chars/pigeon/pigeon.png"),
+            dirt: new PIXI.Texture.from("assets/environment/dirt/dirt.png"),
+        }
+    }
     }
 
     setupDocument(document) {
