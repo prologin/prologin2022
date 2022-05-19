1c470f5f4de91b043673392e3db871e18a6aa3c2
Simon Scatton <simon.scatton@epita.fr>
2022-05-19 17:46:06 +0200

    gui: added game constants and values

--- gui/game.js
+++ gui/game.js
@@ -1,4 +1,34 @@
+const WINDOW_HEIGHT = 960;
+const WINDOW_WIDTH = 960;
+
+const DEPTH = 2;
+const MAP_SIZE = 20;
+
+const SPRITE_WIDTH = WINDOW_WIDTH / MAP_SIZE;
+const SPRITE_HEIGHT = WINDOW_HEIGHT / MAP_SIZE;
+
+const PigeonColor = {
+    BLUE: 0x9999FF,
+    YELLOW: 0xFFFF00,
+    RED: 0xFF4444,
+    NONE: null,
+};
+
+const Direction = {
+    NORD: 0,
+    SUD: 1,
+    EST: 2,
+    OUEST: 3,
+    HAUT: 4,
+    BAS: 5,
+}
+
+function generateMatrix() {
+    return Array(MAP_SIZE).fill(Array(MAP_SIZE).fill(Array(DEPTH)));
+}
+
 export class Game {
+
 
     constructor() {
         this.app = new PIXI.Application({ width: 800, height: 800 });
