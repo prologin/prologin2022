ef71dab9fddaff89a07a4d20c2f70fec90ee8a87
Simon Scatton <simon.scatton@epita.fr>
2022-05-20 23:45:45 +0200

    gui: action_grandir: finished action constructor

--- gui/actions/action_grandir.js
+++ gui/actions/action_grandir.js
@@ -1,3 +1,10 @@
 class ActionGrandir extends Action {
+    constructor(joueur_id, troupe_id) {
+        this.joueur_id = joueur_id;
+        this.troupe_id = troupe_id;
+    }
 
+    execute(game) {
+
+    }
 }
