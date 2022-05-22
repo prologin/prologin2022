1fe88a55d4019710c632134a07f906ef3776ad1d
Paul Guénézan <paul@guenezan.me>
2022-05-22 23:05:16 +0200

    www: add default easter egg

--- www/static/js/header.js
+++ www/static/js/header.js
@@ -1,19 +1,55 @@
-const LOGO_URL = "/static/img/logo.png";
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright 2018 Alexandre Macabies
 
+function flip_dogo() {
+    let $elem = $(".easter-egg-42");
 
-let placeLogo = () => {
-  let imgTag = document.createElement("img");
-  imgTag.src = LOGO_URL;
-  imgTag.style.width = "100%";
-  imgTag.style.padding = "20px 10px";
-  document.querySelector('[role="complementary"]').prepend(imgTag);
-};
+    if ($elem.hasClass("right"))
+        $elem.removeClass("right");
+    else
+        $elem.addClass("right");
+}
 
-document.addEventListener("DOMContentLoaded", function() {
+function plan_42_event(callback) {
+    const wait_for = (102 - (new Date()).getSeconds()) % 60;
 
+    window.setTimeout(
+        () => {
+            callback();
+            plan_42_event(callback);
+        },
+        1000 * wait_for
+    );
+}
 
-  // Adds the finale logo to the website
-  placeLogo();
+$(function() {
+    $('[role=complementary] h1').text('').html('<object data="/static/img/logo.jpg" style="width:100%; margin-top: 0.5em;" type="image/jpeg" />');
 
+    // let's have some easter egg
+    const p1 = $('<img src="/static/img/rainbow.gif" class="easter-egg" />'),
+        p2 = $('<img src="/static/img/pattern.gif" class="easter-egg" />'),
+        dog = $('<div class="easter-egg-42"><img src="/static/img/dogo.gif" class="dogo" /></div>');
 
-})
+    plan_42_event(flip_dogo);
+
+    $('body').append(p1).append(p2).append(dog);
+    $('.easter-egg').on('click', function() {
+        const p = $(this);
+        if (p.hasClass('visible')) {
+            p.removeClass('visible');
+        } else {
+            p.removeClass('peak').addClass('visible');
+        }
+    });
+
+    [p1, p2].forEach(function (p) {
+        if (Math.random() < .5) {
+            p.css('left', 64 + Math.random() * (document.body.clientWidth - 128 - 64));
+            setTimeout(function() {
+                const p = Math.random() < .5 ? p1 : p2;
+                p.addClass('peak');
+            },
+                1000 * (5 + Math.random() * 10));
+        }
+    });
+});
