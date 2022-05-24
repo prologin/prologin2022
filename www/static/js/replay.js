b8415b4da644c3b8762a9b97c6261397006f273d
Paul Guénézan <paul@guenezan.me>
2022-05-24 19:37:43 +0200

    www: add replay.js

---
+++ www/static/js/replay.js
@@ -0,0 +1,45 @@
+$(function () {
+    let $replay = $('#replay'),
+        $playPause = $('#replay-playpause'),
+        $previous = $('#replay-previous'),
+        $next = $('#replay-next'),
+        $turnLabel = $('#replay-turn-label'),
+        $turnSlider = $('#replay-turn-slider');
+
+    $replay.append('<div id="replay_view"></div>');
+    let $replay_view = $("#replay_view");
+
+
+    $replay_view.hide();
+
+    $.getScript('/static/js/pixi.min.js')
+    .done(function() {
+        $.getScript('/static/js/game.js')
+        .done(function() {
+            let game = create_game();
+            game.addToDOM($replay_view[0]);
+            game.setupAnimation();
+
+            // reveal the UI
+            $replay_view.find('canvas').css({
+                'display': 'block',
+                'max-width': '100%',
+                'max-height': '100%',
+                'margin': 'auto',
+            });
+            $replay_view.fadeIn('fast');
+
+            $turnSlider.change(function (e) {
+                let turnIndex = parseInt($turnSlider.val());
+                $previous.prop('disabled', turnIndex <= 0);
+                $next.prop('disabled', turnIndex > 100);
+                $turnLabel.text(turnIndex);
+
+                // Trigger update iff the event was trigger by the UI
+                if (e.originalEvent)
+                    setTurn(2 * turnIndex);
+            });
+        });
+    });
+
+});
