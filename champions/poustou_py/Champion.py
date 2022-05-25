5682057b4030b1007e2fbaf6781ef7a9ac62d032
Quentin Rataud <quentin.rataud@gmail.com>
2022-05-25 18:14:08 -0400

    Fix Poustou-Champion, score !

--- champions/poustou_py/Champion.py
+++ champions/poustou_py/Champion.py
@@ -1,9 +1,19 @@
 from api import *
 from sys import exit
 
+
+def dprint(*args, **kwargs):
+    if moi() == 0 and False:
+        print(*args, **kwargs)
+
+def get_troupe_by_id(troupe_id):
+    for troupe in troupes_joueur(moi()):
+        if troupe.id == troupe_id:
+            return troupe
+
 def display_map():
     case_str = {
-            type_case.GAZON: '',
+            type_case.GAZON: ' ',
             type_case.BUISSON: '█', 
             type_case.BARRIERE: '╳',
             type_case.NID: 'O',
@@ -14,23 +24,33 @@ def display_map():
     }
     pains_str = [' ', '.', ':', '…']
 
-    parc = [[['  '] * LARGEUR for _ in range(HAUTEUR)] for _ in range(2)]
+    parc = [[[' '] * LARGEUR for _ in range(HAUTEUR)] for _ in range(2)]
 
     for z in range(-1, 1):
         for y in range(HAUTEUR):
             for x in range(LARGEUR):
                 pos = x, y, z
                 case = info_case(pos)
-                pains = pains_str[case.nb_pains]
-                disp = case_str[case.contenu] + pains
+                disp = case_str[case.contenu]
                 parc[z][y][x] = disp
+
+
+    for troupe in troupes_joueur(moi()):
+        for x, y, z in troupe.canards:
+            parc[z][y][x] = 'M'
+
+    for troupe in troupes_joueur(adversaire()):
+        for x, y, z in troupe.canards:
+            parc[z][y][x] = 'A'
+
+
     
     for niveau in parc:
         for ligne in niveau:
             for case in ligne:
-                print(case, end = '')
-            print()
-        print('-----------')
+                dprint(case, end = '')
+            dprint()
+        dprint('-----------')
 
 def nearest(troupe_id, condition):
     """
@@ -39,6 +59,7 @@ def nearest(troupe_id, condition):
     Retourne la position ainsi que la liste des directions à prendre
     """
     
+    dprint("Start nearest")
     visite = [[[False] * LARGEUR for _ in range(HAUTEUR)] for _ in range(2)]
     for z in range(-1, 1):
         for y in range(HAUTEUR):
@@ -55,11 +76,12 @@ def nearest(troupe_id, condition):
     def traversable(x, y, z):
         if not (0 <= x < LARGEUR and 0 <= y < HAUTEUR and -1 <= z <= 0):
             return False
-        return visite[z][y][x]
+        return not visite[z][y][x]
 
-    troupe = troupes_joueur(moi())[troupe_id]
+    troupe = get_troupe_by_id(troupe_id)
     start = troupe.maman
     sx, sy, sz = start
+    visite[sz][sy][sx] = False
     file = [(start, [])]
     deplacements = { 
             direction.NORD : (0, 1, 0),
@@ -72,41 +94,67 @@ def nearest(troupe_id, condition):
     while len(file) > 0:
         position, chemin = file.pop(0)
         if condition(position):
+            dprint("End nearest, found")
             return position, chemin
         x, y, z = position
+        if not traversable(x, y, z):
+            continue
         visite[z][y][x] = True
         for direction_, (dx, dy, dz) in deplacements.items():
             rx, ry, rz = x + dx, y + dy, z + dz
             if not traversable(rx, ry, rz):
                 continue
             file.append(((rx, ry, rz), chemin.copy() + [direction_]))
-    
+    dprint("Not found")
     return None
 
 
 
-
-target = [None, None]
+ids = []
+target = [None, None, None]
 # Fonction appelée au début de la partie.
 def partie_init():
-    for troupe_id in range(2):
+    for troupe in troupes_joueur(moi()):
+        ids.append(troupe.id)
+    for troupe_id in ids:
         target[troupe_id] = nearest(troupe_id, lambda position: info_case(position).contenu == type_case.NID)
 
 
 
 # Fonction appelée à chaque tour.
 def jouer_tour():
-    for troupe_id in range(2):
-        troupe = troupes_joueur(moi())[troupe_id]
+    dprint("jouer tour")
+    for troupe_id in ids:
+        dprint(troupe_id, "start")
+        troupe = get_troupe_by_id(troupe_id)
+        if target[troupe_id] is None:
+            dprint("Reset !")
+            for troupe_id in ids:
+                target[troupe_id] = nearest(troupe_id, lambda position: info_case(position).contenu == type_case.NID)
+            continue
+
         arrivee, chemin = target[troupe_id]
         while troupe.pts_action > 0:
-            if avancer(troupe_id, chemin.pop(0)) != erreur.ok:
-                exit(1)
+            dprint(troupe.pts_action, "restants")
+            if len(chemin) > 0:
+                if avancer(troupe_id, chemin.pop(0)) != erreur.OK:
+                    exit(1)
             if len(chemin) == 0:
+                dprint("End reached")
                 if info_case(arrivee).contenu == type_case.NID:
+                    dprint("On a nest")
                     target[troupe_id] = nearest(troupe_id, lambda position: info_case(position).nb_pains > 0)
                 else:
+                    dprint("On a pain")
                     target[troupe_id] = nearest(troupe_id, lambda position: info_case(position).contenu == type_case.NID and info_nid(position) == moi() + 1)
+                if target[troupe_id] is None:
+                    dprint("Not found, exit !")
+                    break
+                arrivee, chemin = target[troupe_id]
+                if len(chemin) == 0:
+                    dprint("Nothing to do, exit !")
+                    break
+            troupe = get_troupe_by_id(troupe_id)
 
 
 # Fonction appelée à la fin de la partie.
