3477ddda837e48623a28ee668c60e0763245240a
Antoine Martin <antoine.martin@prologin.org>
2022-05-28 15:47:58 +0200

    Revert "rules: fix an error in position.hh"
    This reverts commit 4f40e12d5ce7cb6bed47c3cc4d99f6680207f297.

---
+++ src/position.hh
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (c) 2012-2020 Association Prologin <association@prologin.org>
+#pragma once
+
+#include <utils/log.hh>
+
+#include "constant.hh"
+
+inline bool operator==(const position& lhs, const position& rhs)
+{
+    return lhs.ligne == rhs.ligne && lhs.colonne == rhs.colonne &&
+           lhs.niveau == rhs.niveau;
+}
+
+inline bool operator!=(const position& lhs, const position& rhs)
+{
+    return !(lhs == rhs);
+}
+
+inline position& operator+=(position& lhs, position rhs)
+{
+    lhs.colonne += rhs.colonne;
+    lhs.ligne += rhs.ligne;
+    lhs.niveau += rhs.niveau;
+    return lhs;
+}
+
+inline position operator+(position lhs, position rhs)
+{
+    return lhs += rhs;
+}
+
+inline bool inside_map(const position& pos)
+{
+    return pos.ligne >= 0 && pos.ligne < HAUTEUR && pos.colonne >= 0 &&
+           pos.colonne < LARGEUR && pos.niveau <= 0 && pos.niveau >= -1;
+}
+
+inline position get_delta_pos(const direction& dir)
+{
+    position delta = {.colonne = 0, .ligne = 0, .niveau = 0};
+    switch (dir)
+    {
+    case NORD:
+        delta.ligne = 1;
+        break;
+    case SUD:
+        delta.ligne = -1;
+        break;
+    case EST:
+        delta.colonne = 1;
+        break;
+    case OUEST:
+        delta.colonne = -1;
+        break;
+    case HAUT:
+        delta.niveau = 1;
+        break;
+    case BAS:
+        delta.niveau -= 1;
+        break;
+    }
+    return delta;
+}
+
+inline direction inverse_dir(direction dir)
+{
+    switch (dir)
+    {
+    case NORD:
+        return SUD;
+    case SUD:
+        return NORD;
+    case EST:
+        return OUEST;
+    case OUEST:
+        return EST;
+    case HAUT:
+        return BAS;
+    case BAS:
+        return HAUT;
+    }
+
+    __builtin_unreachable();
+}
+
+inline direction flatten_dir(direction dir)
+{
+    switch (dir)
+    {
+    case HAUT:
+        return NORD;
+    case BAS:
+        return SUD;
+    }
+}
+
+inline direction clockwise_dir(direction dir)
+{
+    switch (dir)
+    {
+    case NORD:
+        return EST;
+    case SUD:
+        return OUEST;
+    case EST:
+        return SUD;
+    case OUEST:
+        return NORD;
+    case HAUT:
+        return BAS;
+    case BAS:
+        return HAUT;
+    }
+
+    __builtin_unreachable();
+}
